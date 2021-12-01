52d82eea828a23196f76c9a510a6e34b2db81890
winny1212 <maomaodragon1212@gmail.com>
2021-12-01 14:37:23 +1100

    the main page structure,impport react icon and styling

---
+++ review-dev/src/Components/Review.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Review from './Components/Review'
+import { IconName } from "react-icons/go";
+
+export default function Review() {
+    return (
+        <main>
+            <section className='container'>
+                <div class='title'>
+                   <h2>our reviews</h2>
+                   <div className='underline'></div>
+                </div>
+            </section>
+        </main>
+    )
+}
