16e2b816da682d2b3917d16dae01f8c645ac276b
winny1212 <maomaodragon1212@gmail.com>
2021-12-03 11:34:43 +1100

    set up the structure

---
+++ navbar/src/Navbar.js
@@ -0,0 +1,12 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars,FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+
+export default function Navbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
